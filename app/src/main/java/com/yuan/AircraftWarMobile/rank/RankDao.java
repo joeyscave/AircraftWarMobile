820d97319e7105609fed2520726d7f749908572e
Alannzer <1720943819@qq.com>
2022-05-17 21:46:56 +0800

    offline rank | code transplant

--- app/src/main/java/com/yuan/AircraftWarMobile/rank/RankDao.java
+++ app/src/main/java/com/yuan/AircraftWarMobile/rank/RankDao.java
@@ -1,10 +1,5 @@
 package com.yuan.AircraftWarMobile.rank;
 
 public interface RankDao {
-    public void store(String mode);
-    public void load(String mode);
-    public void add(String name,int score,String date);
-    public void remove(int rank);
-    public void clear();
-    public String[][] toTable();
+    public void insert(Record record);
 }
--- app/src/main/java/com/yuan/AircraftWarMobile/rank/RankDaoImp.java
+++ app/src/main/java/com/yuan/AircraftWarMobile/rank/RankDaoImp.java
@@ -1,13 +1,14 @@
 package com.yuan.AircraftWarMobile.rank;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 import com.yuan.AircraftWarMobile.settings.Settings;
 
-import java.io.EOFException;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,80 +16,64 @@ import java.util.List;
 public class RankDaoImp implements RankDao {
 
     private List<Record> rank = new LinkedList<>();
+    private MyHelper helper;
+    private String tableName;
 
-    public RankDaoImp() {
-        String[] mode={"easy","normal","hard"};
-        this.load(mode[Settings.backGroundIndex/2]);
-    }
-
-    @Override
-    public String toString() {
-        String rankString =
-                "**********************\n" +
-                        "       得分排行榜\n" +
-                        "**********************\n";
-        int index = 1;
-        for (Record record : rank) {
-            rankString += "第" + index + "名：" + record + "\n";
-            index++;
-        }
-        return rankString;
-    }
-
-    @Override
-    public String[][] toTable() {
-        if (rank == null) return null;
-        String[][] tableData = new String[rank.size()][];
-        int index = 1;
-        for (Record record : rank) {
-            String[] recordString = {Integer.toString(index), record.getName(), Integer.toString(record.getScore()), record.getDate()};
-            tableData[index - 1] = recordString;
-            index++;
-        }
-        return tableData;
-    }
-
-    /**
-     * 从文件读入javabean
-     */
-    @Override
-    public void load(String mode) {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("src\\edu\\hitsz\\rank\\rank_"+mode+".txt"))) {
-            rank = (LinkedList<Record>) ois.readObject();
-        } catch (EOFException e) {
-            //忽略
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
+    public RankDaoImp(Context context) {
+        helper = new MyHelper(context);
+        switch (Settings.backGroundIndex / 2) {
+            case 0:
+                tableName = "easyRank";
+                break;
+            case 1:
+                tableName = "normalRank";
+                break;
+            case 2:
+                tableName = "hardRank";
+                break;
+            default:
         }
     }
 
-    /**
-     * 从javabean写入文件
-     */
-    @Override
-    public void store(String mode) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src\\edu\\hitsz\\rank\\rank_"+mode+".txt"))) {
-            oos.writeObject(rank);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public void insert(Record record) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        //db.execSQL("delete from " + tableName);//通过该指令清空数据库
+        ContentValues values = new ContentValues();
+        values.put("id",record.getId());
+        values.put("name", record.getName());
+        values.put("score", record.getScore());
+        values.put("date", record.getDate());
+        db.insert(tableName, null, values);
+        db.close();
+    }
+
+    public List<Record> queryAll() throws IOException {
+        SQLiteDatabase db=helper.getWritableDatabase();
+        Cursor c=db.query(tableName,null,null,null,null,null,"score DESC");
+        List<Record> records = new LinkedList<>();
+        while (c.moveToNext()){
+            int id=c.getInt(c.getColumnIndex("id"));
+            String name=c.getString(c.getColumnIndex("name"));
+            int score=c.getInt(c.getColumnIndex("score"));
+            String date=c.getString(c.getColumnIndex("date"));
+            records.add(new Record(id ,name, score, date));
         }
+        db.execSQL("delete from " + tableName);
+        sortRank(records);
+        c.close();
+        db.close();
+        return records;
     }
 
-    @Override
-    public void add(String name, int score, String date) {
-        rank.add(new Record(score, name, date));
-        System.out.println("----------");
-        Collections.sort(rank);
-    }
-
-    @Override
-    public void remove(int row) {
-        this.rank.remove(row);
-        Collections.sort(this.rank);
-    }
-
-    @Override
-    public void clear() {
-        rank.clear();
+    public List<Record> sortRank (List<Record> records) throws IOException{
+        Collections.sort(records);
+        int ranknum = 0;
+        for(Record u:records){
+            ranknum += 1;
+            u.setId(ranknum);
+            insert(u);
+            System.out.println("排行" + u.getId()+"\t"+u.getName()+"\t"+u.getScore()+"\t"+u.getDate());
+        }
+        return records;
     }
 }
